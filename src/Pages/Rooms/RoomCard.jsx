9ce66f581aa29d2598a14a00a5cf34a0d5cfe60a
JannaturNur08 <nur@playense.com>
2023-11-06 16:53:07 +0600

    added price slider in room page

--- src/Pages/Rooms/RoomCard.jsx
+++ src/Pages/Rooms/RoomCard.jsx
@@ -4,11 +4,16 @@ const RoomCard = ({room}) => {
     const {category_name,rooms,room_size,price,reviews,room_images} = room;
 
     return (
-        <div>
+        <div className="space-y-2">
             <img src={rooms.room_images[0]} alt="" />
-            <h2>{category_name}</h2>
+            <h2 className="font-mercellus text-2xl">{category_name}</h2>
             <p>Available Rooms:  {rooms.room_number}</p>
-            <p>Room Size :{rooms.room_size}</p>
+            <div className="flex flex-row gap-3">
+                <p>{rooms.room_size}</p>
+                <p>{rooms.guest} Guests </p>
+                <p>{rooms.bed} Beds </p>
+             <p>   {rooms.bath} Baths </p>
+            </div>
             <p>Price : $ {rooms.price}</p>
             
             <p>Review : {reviews.length}</p>
--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,23 +1,32 @@
 
+import { useState } from "react";
 import useRooms from "../../hooks/useRooms";
 import RoomCard from "./RoomCard";
 
 const Rooms = () => {
 	
     const rooms = useRooms();
+    const [maxPrice, setMaxPrice] = useState(500);
+    const filteredRoomsWithPrice = rooms.filter(room => room.rooms.price <= maxPrice);
+const handleSliderChange = e => {
+    const newMaxPrice = parseInt(e.target.value,10);
+    console.log(newMaxPrice);
+    setMaxPrice(newMaxPrice);
+}
 	
 	
 	
 	return (
 		<div>
 			<h2>Room Page</h2>
+            <input type="range" min={0} max={500}  value={maxPrice} onChange={handleSliderChange} className="range range-success w-1/2" />
             <div className="text-center">
            
 			
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {
-                    rooms.map(room => <RoomCard room={room} key={room._id}></RoomCard>)
+                    filteredRoomsWithPrice.map(room => <RoomCard room={room} key={room._id}></RoomCard>)
                 }
             </div>
 		</div>
