3a6a74946ea2264bec09b6ac145331c08de57335
JannaturNur08 <nur@playense.com>
2023-11-09 18:18:36 +0600

    404 page image added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -7,6 +7,7 @@ const ErrorPage = () => {
 		<div id="error-page" className="mx-auto container text-center mt-20">
 			<h1 className="text-5xl">Oops!</h1>
 			<p className="text-2xl">Sorry, an unexpected error has occurred.</p>
+			<img src="https://i.ibb.co/9VmY926/oops-404-error-with-broken-robot-concept-illustration-114360-5529.jpg" alt="" />
 			<p className="text-xl">
 				<i>{error.statusText || error.message}</i>
 			</p>
